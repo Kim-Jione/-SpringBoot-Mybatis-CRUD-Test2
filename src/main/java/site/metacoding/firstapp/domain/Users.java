3ffa12d1062328a0def790f7d9108a753d8f3665
Kimjiwon <hghg1036@naver.com>
2022-10-29 19:29:53 +0900

    초기세팅 완료

---
+++ src/main/java/site/metacoding/firstapp/domain/Users.java
@@ -0,0 +1,23 @@
+package site.metacoding.firstapp.domain;
+
+import java.sql.Timestamp;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@Getter
+public class Users {
+
+	private Integer id;
+	private String username;
+	private String password;
+	private String email;
+	private Timestamp createdAt;
+
+	public Users(String username, String password, String email) {
+		this.username = username;
+		this.password = password;
+		this.email = email;
+	}
+}
---
+++ src/main/java/site/metacoding/firstapp/domain/UsersDao.java
@@ -0,0 +1,13 @@
+package site.metacoding.firstapp.domain;
+
+import java.util.List;
+
+
+public interface UsersDao {
+	public void insert(Users users); 
+	public Users findById(Integer id);
+	public Users findByUsername(String username);
+	public List<Users> findAll();
+	public void update(Users users); 
+	public void delete(Integer id);	
+}
