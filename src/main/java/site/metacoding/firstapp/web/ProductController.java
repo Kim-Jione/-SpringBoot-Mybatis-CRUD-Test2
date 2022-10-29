e81194e8d86ad24d4b1191ca16dd37d6ffa0bd25
Kimjiwon <hghg1036@naver.com>
2022-10-29 20:24:37 +0900

    상품 상세보기 구현완료

--- src/main/java/site/metacoding/firstapp/web/ProductController.java
+++ src/main/java/site/metacoding/firstapp/web/ProductController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 import lombok.RequiredArgsConstructor;
 import site.metacoding.firstapp.domain.Product;
@@ -19,8 +20,12 @@ public class ProductController {
     public String list(Model model) {
     	List<Product> product = productDao.findAll();
     	model.addAttribute("product", product);
-        return "product/productList";
+        return "product/listForm";
     }
 	
-	
+	 @GetMapping("/product/{id}")
+	    public String detail(@PathVariable Integer id, Model model) {
+	    	model.addAttribute("detail", productDao.findById(id));
+	        return "product/detailForm";
+	    }
 }
